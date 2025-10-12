dc53c64cf050a3c4257216f191b6aa680a1e5ca3
gufrantestuser <gufrantestuser@gmail.com>
2025-10-12 14:40:42 +0000

    Fix the code

--- pathosync-ui/src/main.tsx
+++ pathosync-ui/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './styles/globals.css'
+import './index.css'
 
 // Error handling for development
 if (import.meta.env.DEV) {
